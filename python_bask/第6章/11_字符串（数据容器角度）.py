739fdade43b08a6bd8c8f6af7c830545f48ee5ff
lilu <542634879@qq.com>
2025-12-19 13:56:37 +0800

    提交已有案例

---
+++ python_bask/第6章/11_字符串（数据容器角度）.py
@@ -0,0 +1,14 @@
+# 字符串的下标
+# msg = 'welcome to python'
+# print(msg)
+# print(msg[3])
+
+
+# 字符串中的字符，不可修改
+# msg1 = 'welcome to my world'
+# msg1[1] = 'hello'
+# print(msg1)
+
+# 字符串不能嵌套
+msg2 = 'welcome to "yoyo" my world'
+print(msg2)
